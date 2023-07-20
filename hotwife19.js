6b08b0d1819bdb2f4d2652b8295affed122cd885
brasilisnet <83701053+brasilisnet@users.noreply.github.com>
2023-07-20 18:20:05 +0900

    Add files via upload

---
+++ hotwife19.js
@@ -0,0 +1,10 @@
+//Javascript Created by Computerhope https://www.computerhope.com/
+//store the quotations in arrays
+var images = [],
+index = 0;
+images[0] = "<a target='_blank' href='https://shope.ee/3VDNHWRl8C'><img border='0' src='https://i.imgur.com/ZD6IAGI.jpg' width='100%' height='auto'></a>";
+images[1] = "<a target='_blank' href='https://shope.ee/3VDNHWRl8C'><img border='0' src='https://i.imgur.com/gt7figI.jpg' width='100%' height='auto'></a>";
+images[2] = "<a target='_blank' href='https://shope.ee/3VDNHWRl8C'><img border='0' src='https://i.imgur.com/YVZun4D.jpg' width='100%' height='auto'></a>";
+index = Math.floor(Math.random() * images.length);
+document.write(images[index]);
+//done
